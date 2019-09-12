4aaf96e837a86831929b64beac60fe848305db76
julianriegler <45983078+jriegs@users.noreply.github.com>
2019-09-12 17:29:19 -0500

    organize gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,9 @@
-var gulp = require('gulp'),
-watch = require('gulp-watch');
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
-gulp.task('default', function() {
-    console.log('hooray, you created a gulp task');
-});
 
-gulp.task('html', function() {
-    console.log('imagine something useful being done to your html');
-});
 
-gulp.task('styles', function() {
-    console.log('imagine sass or postcss tasks running here');
-});
 
-gulp.task('watch', function() {
-    watch('./app/index.html', function() {
-        gulp.start('html');
-    });
 
-    watch('./app/assets/**/*.css', function() {
-        gulp.start('styles');
-    })
-});
+
+
